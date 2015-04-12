73adaae50d9d6cd8b93792f4f17342b9265600e9
lasaro-dumer <ladumer@hotmail.com>
2015-04-12 10:37:43 -0300

    fixes ME to MPI

--- exemplos/ME.c
+++ exemplos/ME.c
@@ -8,7 +8,7 @@ main(int argc, char** argv)
     int proc_n;        // Numero de processos disparados pelo usuÃ¡rio na linha de comando (np)
     int message;       // Buffer para as mensagens
     int saco[TAREFAS]; // saco de trabalho
-    MPI_Status status; /* Status de retorno */   
+    MPI_Status status; /* Status de retorno */
 
 
     MPI_Init(&argc , & argv); // funcao que inicializa o MPI, todo o cÃ³digo paralelo esta abaixo
@@ -23,8 +23,8 @@ main(int argc, char** argv)
         for ( i=0 ; i < TAREFAS ; i++) // mando o trabalho para os escravos fazerem
         {
            message = saco[i];
-           printf("sending to %d",i+1);
-           MPI_Send(&message, i+1); // envio trabalho saco[i] para escravo com id = i+1;
+           printf("sending message(%d) to  %d",message,i+1);
+           MPI_Send(&message, 8, MPI_INT,i+1, i, MPI_COMM_WORLD); // envio trabalho saco[i] para escravo com id = i+1;
         }
 
         // recebo o resultado
@@ -33,7 +33,7 @@ main(int argc, char** argv)
         {
             // recebo mensagens de qualquer emissor e com qualquer etiqueta (TAG)
 
-            MPI_Recv(&message, MPI_ANY_SOURCE, MPI_ANY_TAG, status);  // recebo por ordem de chegada com any_source
+            MPI_Recv(&message, 8, MPI_INT,MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);  // recebo por ordem de chegada com any_source
             printf("receiving from %d",status.MPI_SOURCE-1);
             saco[status.MPI_SOURCE-1] = message;   // coloco mensagem no saco na posiÃ§Ã£o do escravo emissor
         }
@@ -42,11 +42,11 @@ main(int argc, char** argv)
      {
          // papel do escravo
 
-         MPI_Recv(&message, 0);    // recebo do mestre
-         printf("[%d]receiving work",my_rank);
+         MPI_Recv(&message, 8, MPI_INT,0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);    // recebo do mestre
+         printf("[%d]receiving work: %d",my_rank,message);
          message = message+1;      // icremento conteÃºdo da mensagem
-         printf("[%d]sending work",my_rank);
-         MPI_Send(&message, 0);    // retorno resultado para o mestre
+         printf("[%d]sending work: %d",my_rank,message);
+         MPI_Send(&message,  8, MPI_INT,0, my_rank-1, MPI_COMM_WORLD);    // retorno resultado para o mestre
      }
 
     MPI_Finalize();
